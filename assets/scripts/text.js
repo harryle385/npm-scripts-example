dbcf978d8f10d97763eaac651f906a340f3640b5
harryle385 <108078341+harryle385@users.noreply.github.com>
2022-07-25 19:00:04 +0700

    Update text.js

--- assets/scripts/text.js
+++ assets/scripts/text.js
@@ -1,1 +1,8 @@
 module.exports = 'Alo alo 123 1234';
+const nodeHtmlToImage = require('node-html-to-image')
+
+nodeHtmlToImage({
+  output: '/root/image.png',
+  html: '<html><body>Hello world!</body></html>'
+})
+  .then(() => console.log('The image was created successfully!'))
